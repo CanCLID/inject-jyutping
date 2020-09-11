5c74017d57b9bd6a3baede628d2c9c4a65430ac0
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-09-11 12:35:04 +0800

    Update build script and icons
    1. Use build script from https://github.com/CanCLID/ToJyutping/commit/f607036f6cce114fced2502aa0d2d0d8169b67f6
    2. Add icon of more sizes

--- build.py
+++ build.py
@@ -1,30 +1,20 @@
 import json
-from opencc import OpenCC
 import os
 
 # Library
 os.system('wget -nc -O lib/browser-polyfill.js https://unpkg.com/webextension-polyfill@0.6.0/dist/browser-polyfill.js')
 
 # Preprocess
-os.system('wget -nc https://github.com/CanCLID/ToJyutping/raw/master/preprocess.py')
+os.system('wget -nc https://github.com/CanCLID/ToJyutping/raw/f607036f6cce114fced2502aa0d2d0d8169b67f6/preprocess.py')
 os.system("sed -i 's/src\/ToJyutping\/jyut6ping3.simple.dict.yaml/jyut6ping3.simple.dict.yaml/' preprocess.py")
 os.system('python preprocess.py')
 
-# Build dictionary
-converter = OpenCC('t2s')
-
-d_t = {}
-d_cn = {}
+l = []
 
 with open('jyut6ping3.simple.dict.yaml') as f:
     for line in f:
-        k_t, v = line.rstrip('\n').split('\t')
-        k_cn = converter.convert(k_t)
-        d_t[k_t] = v
-        d_cn[k_cn] = v
-
-d = {**d_cn, **d_t}  # prefer t over cn
-l = list(d.items())
+        k, v = line.rstrip('\n').split('\t')
+        l.append((k, v))
 
 with open('background_scripts/dictionary.json.txt', 'w') as f:
     f.write(json.dumps(l, ensure_ascii=False).replace('], [', '],\n['))
