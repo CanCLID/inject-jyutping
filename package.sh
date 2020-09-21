d10abb3b75a041657bd08ffae4b87b7f0dae4cce
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-09-21 11:07:04 +0800

    Update popup
    1. Shrink font size in the popup window.
    2. Add a prompt to let users refresh the page for the change to take effect.
    3. Add Chinese translation of “inject automatically” for all languages.

--- package.sh
+++ package.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+rm -rf inject-jyutping.zip
 zip -r inject-jyutping.zip _locales background_scripts content_scripts icons lib popup manifest.json
--- popup/index.js
+++ popup/index.js
@@ -1,11 +1,12 @@
 /* Initialize state */
 (async () => {
     document.documentElement.lang = browser.i18n.getMessage('langCode');
-    document.getElementById('checkboxText').innerText = browser.i18n.getMessage('popupCheckboxText');
+    document.getElementById('checkboxText').innerHTML = browser.i18n.getMessage('popupCheckboxText');
     document.getElementById('extensionEnabled').checked = ((await browser.storage.local.get('enabled'))['enabled'] !== false) ? true : false;
 })();
 
 /* Handle state change */
 document.getElementById('extensionEnabled').addEventListener('click', () => {
     browser.storage.local.set({enabled: document.getElementById('extensionEnabled').checked});
+    document.getElementById('refreshPromptText').innerHTML = browser.i18n.getMessage('refreshPromptText');
 });
