638b80a8497a9ebe98be68669afab17379c2008b
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-09-27 11:12:27 +0800

    Update ToJyutping to https://github.com/CanCLID/ToJyutping/commit/54945d4cf1fc86858acff7b1dd6f3f13dc838bf0

--- build.py
+++ build.py
@@ -5,17 +5,17 @@ import os
 os.system('wget -nc -O lib/browser-polyfill.js https://unpkg.com/webextension-polyfill@0.6.0/dist/browser-polyfill.js')
 
 # Preprocess
-os.system('wget -nc https://github.com/CanCLID/ToJyutping/raw/f607036f6cce114fced2502aa0d2d0d8169b67f6/preprocess.py')
+os.system('wget -nc https://github.com/CanCLID/ToJyutping/raw/54945d4cf1fc86858acff7b1dd6f3f13dc838bf0/preprocess.py')
 os.system("sed -i 's/src\/ToJyutping\/jyut6ping3.simple.dict.yaml/jyut6ping3.simple.dict.yaml/' preprocess.py")
 os.system('python preprocess.py')
 
 l = []
 
 with open('jyut6ping3.simple.dict.yaml') as f:
-    for line in f:
-        k, v = line.rstrip('\n').split('\t')
-        l.append((k, v))
+	for line in f:
+		k, v = line.rstrip('\n').split('\t')
+		l.append((k, v))
 
-with open('background_scripts/dictionary.json.txt', 'w') as f:
-    f.write(json.dumps(l, ensure_ascii=False).replace('], [', '],\n['))
-    f.write('\n')  # Add line break at the end of file
+with open('background_scripts/dictionary.json.txt', 'w') as f:  # *.json.txt: See mozilla/addons-linter#1700
+	f.write(json.dumps(l, ensure_ascii=False).replace('], [', '],\n['))
+	f.write('\n')  # Add line break at the end of file
