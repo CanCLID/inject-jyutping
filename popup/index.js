dc9e3b6d039ba2dcddc0ff612a6645a1d2a7f67c
graphemecluster <graphemecluster@gmail.com>
2024-07-25 09:59:32 +0800

    Use to-jyutping Package, Strict Typing, Migrate to Manifest Version 3 (#8)
    * Use to-jyutping Package, Strict Typing
    
    * Migrate to Manifest Version 3

--- popup/index.js
+++ popup/index.js
@@ -1,12 +1,20 @@
+import '/node_modules/webextension-polyfill/dist/browser-polyfill.min.js';
+
+const i = browser.i18n.getMessage;
+
+const autoInjectNativeText = /** @type {HTMLDivElement} */ (document.getElementById('auto-inject-native-text'));
+const autoInjectCheckbox = /** @type {HTMLInputElement} */ (document.getElementById('auto-inject-checkbox'));
+const refreshPromptText = /** @type {HTMLParagraphElement} */ (document.getElementById('refresh-prompt-text'));
+
 /* Initialize state */
 (async () => {
-    document.documentElement.lang = browser.i18n.getMessage('langCode');
-    document.getElementById('checkboxText').innerHTML = browser.i18n.getMessage('popupCheckboxText');
-    document.getElementById('extensionEnabled').checked = (await browser.storage.local.get('enabled'))['enabled'] !== false;
+    document.documentElement.lang = i('langCode');
+    autoInjectNativeText.textContent = i('popupCheckboxText');
+    autoInjectCheckbox.checked = (await browser.storage.local.get('enabled'))['enabled'] !== false;
 })();
 
 /* Handle state change */
-document.getElementById('extensionEnabled').addEventListener('click', () => {
-    browser.storage.local.set({ enabled: document.getElementById('extensionEnabled').checked });
-    document.getElementById('refreshPromptText').innerHTML = browser.i18n.getMessage('refreshPromptText');
+autoInjectCheckbox.addEventListener('click', () => {
+    browser.storage.local.set({ enabled: autoInjectCheckbox.checked });
+    refreshPromptText.innerHTML = i('refreshPromptText');
 });
